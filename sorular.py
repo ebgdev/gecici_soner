43556a71a0f85a92f7cd7ac52f761d574ef12c68
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2025-05-23 13:12:59 +0300

    sorular.py get restored.

---
+++ sorular.py
@@ -0,0 +1,27 @@
+# def isPowTwo(num:int) -> bool:
+#     i = 0
+#     while i < num:
+#         if 2 **i == num:
+#             return True
+#         i+=1
+#     return False
+
+
+
+
+# # 2^x == num --> True , or: 1,2,4,8,64,256,1024
+# # 3,5
+
+
+# print(isPowTwo(16))
+
+
+
+def isPowTwo(num:int) -> bool:
+    for i in range(num):
+        if 2**i == num:
+            return True
+    return False
+    
+    
+print(isPowTwo(32))
