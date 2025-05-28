abb91fdb3b2b88c03b8497ec1d38681c5ac619e7
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2025-05-28 14:36:14 +0300

    pickevens get updated.

--- pick_evens.py
+++ pick_evens.py
@@ -1,8 +1,12 @@
-from icecream import ic
-lst = ["kucuk","kazak","ada","soner","erfan"]
+lst = [12,19,20.1,9.9999999999999999,
+       'soner','samsun',21,10.00001
+    ]
 
-def isPalindorme(word):
-    return word == word[::-1]
-        
-for word in lst:
-    ic(word,isPalindorme(word))
+def sayilar(my_list):
+    res = []
+    for item in my_list:
+        if (item % 2 == 0) and item> 10 and type(item) == int:
+            res.append(item)
+    res(res)
+    
+print(sayilar(lst))
